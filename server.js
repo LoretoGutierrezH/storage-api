0e6af1d4d3cfdb7018699013224f8107260c98f9
Loreto <loretocgh@gmail.com>
2022-07-10 15:41:05 -0400

    dotenv global require

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const http = require('http')
 const mongoose = require('mongoose')
 const app = require('./config/app')
+const dotenv = require('dotenv')
 //const dotenv = require('dotenv').config()  .config() sets process.env to default .env file
 
 const server = http.createServer(app)
 if (process.env.NODE_ENV === 'prod') {
-  require('dotenv').config({path: `${__dirname}/prod.env`})
+  dotenv.config({path: `${__dirname}/prod.env`})
 }
 
 if (process.env.NODE_ENV === 'dev') {
-  require('dotenv').config({path: `${__dirname}/dev.env`})
+  dotenv.config({path: `${__dirname}/dev.env`})
 }
 const mongoURL = process.env.MONGODB_URL.replace('<password>', process.env.MONGODB_PASSWORD)
 
