53e7da010e3f92bcb48bec96e7f7c6955f37dc34
Loreto <loretocgh@gmail.com>
2022-08-23 09:06:12 -0400

    some commit

--- server.js
+++ server.js
@@ -5,17 +5,18 @@ const dotenv = require('dotenv')
 
 const server = http.createServer(app)
 if (process.env.NODE_ENV === 'prod') {
-  dotenv.config({path: `${__dirname}/prod.env`})
+  dotenv.config({ path: `${__dirname}/prod.env` })
 }
 
 if (process.env.NODE_ENV === 'dev') {
-  dotenv.config({path: `${__dirname}/dev.env`})
+  dotenv.config({ path: `${__dirname}/dev.env` })
 }
 const mongoURL = process.env.MONGODB_URL.replace('<password>', process.env.MONGODB_PASSWORD)
 
 mongoose.connect(mongoURL).then(() => {
   console.log('DB connection successful')
-  server.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`)
+  const port = process.env.PORT || 5000
+  server.listen(port, () => {
+    console.log(`Server running on port ${port}`)
   })
 })
